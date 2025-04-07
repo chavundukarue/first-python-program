352616fb54297b25106999de461ee587badc8222
Your Name <ruthmajoni53@gmail.com>
2025-04-07 23:09:06 +1400

    payslip.py

---
+++ ruth.py
@@ -0,0 +1,18 @@
+def add(x,y):
+
+    return x+ y
+def subtract(x,y):
+    return x- y
+def multiply(x,y):
+    return x*y
+print("Addition")
+print("subtraction")
+print("multiply")
+
+
+
+
+
+
+
+
